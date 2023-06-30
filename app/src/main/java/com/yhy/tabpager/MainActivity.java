9fa28151fe957246a07a9985b1016a6511536a32
颜洪毅 <yhyzgn@gmail.com>
2023-06-30 13:04:29 +0800

    feat:update

--- app/src/main/java/com/yhy/tabpager/MainActivity.java
+++ app/src/main/java/com/yhy/tabpager/MainActivity.java
@@ -2,44 +2,24 @@ package com.yhy.tabpager;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.tv_tpg).setOnClickListener(this);
-        findViewById(R.id.tv_tpg_custom).setOnClickListener(this);
-        findViewById(R.id.tv_nav).setOnClickListener(this);
-        findViewById(R.id.tv_hybrid).setOnClickListener(this);
-        findViewById(R.id.tv_recycler).setOnClickListener(this);
+        findViewById(R.id.tv_tpg).setOnClickListener(v -> to(TpgActivity.class));
+        findViewById(R.id.tv_tpg_custom).setOnClickListener(v -> to(TpgCustomActivity.class));
+        findViewById(R.id.tv_nav).setOnClickListener(v -> to(NavActivity.class));
+        findViewById(R.id.tv_hybrid).setOnClickListener(v -> to(HybridActivity.class));
+        findViewById(R.id.tv_recycler).setOnClickListener(v -> to(RecyclerActivity.class));
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.tv_tpg:
-                startActivity(new Intent(this, TpgActivity.class));
-                break;
-            case R.id.tv_tpg_custom:
-                startActivity(new Intent(this, TpgCustomActivity.class));
-                break;
-            case R.id.tv_nav:
-                startActivity(new Intent(this, NavActivity.class));
-                break;
-            case R.id.tv_hybrid:
-                startActivity(new Intent(this, HybridActivity.class));
-                break;
-            case R.id.tv_recycler:
-                startActivity(new Intent(this, RecyclerActivity.class));
-                break;
-            default:
-                break;
-        }
+    private void to(Class<?> activity) {
+        startActivity(new Intent(this, activity));
     }
 }
